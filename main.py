fbfea1bc5f79de7039714a0474b4f94702118f75
lko9911 <lko991111@gmail.com>
2025-05-13 22:06:22 +0900

    modified:   detected_objects.json 	new file:   ggul_bot/Robot_Operation.py 	modified:   ggul_bot/__pycache__/Raspberry_Websocket.cpython-310.pyc 	new file:   ggul_bot/__pycache__/Robot_Operation.cpython-310.pyc 	modified:   joint_states_log.json 	modified:   main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ggul_bot.Strawberry_Vision import detect_and_save
 from ggul_bot.Coordinate_Transformations import load_detected_objects_test, print_detected_objects_test, transform_coordinates60
 from ggul_bot.Classify_Disease import detect_and_show
 from ggul_bot.Raspberry_Websocket import send_detected_objects, start_joint_state_server
+#from ggul_bot.Robot_Operation import read_and_send_joint_values, process_joint_angles
 import asyncio
 import json
 
@@ -21,6 +22,7 @@ async def main_loop():
         while True:
             print(f"\n========== [{i}번째 주기 시작] ==========")
 
+            #-----------------Strawberry-Vision 실행부------------------#
             print(f"[{i}] YOLO 탐지 및 3D 위치 추정 중...")
             detect_and_save(model_path=yolo_path, npz_path=npz_path, save_path=json_path, time_interval=10000)
 
@@ -38,11 +40,15 @@ async def main_loop():
             # print(f"[{i}] 질병 분류 모델 실행 중...")
             # detect_and_show(model_path=yolo_path, npz_path=npz_path, keras_path=keras_path, time_end=10)
 
+            #-----------------ROS2에 내용 전송------------------#
             print(f"[{i}] WebSocket을 통해 ROS2에 전송 중...")
             try:
                 await send_detected_objects()
             except Exception as e:
                 print(f"[{i}] WebSocket 통신 중 오류 발생: {e}")
+
+
+            #-----------------ROS2에 내용 수신신부------------------#
 
             # 큐에서 수신된 모든 조인트 데이터 수집
             joint_data_list = []
@@ -63,6 +69,10 @@ async def main_loop():
                         f.write(json.dumps(jd) + "\n")
             else:
                 print(f"[{i}] 이번 주기에는 조인트 상태가 도착하지 않았습니다.")
+
+            #-----------------로봇팔 동작부------------------#
+            log_file_path = "joint_states_log.json"
+            #read_and_send_joint_values(log_file_path)
 
             i += 1
             await asyncio.sleep(10)  # 주기적 실행
